b984601956e1e8cf3e9b3a4cb8fc1505c500c800
200613775 <patelnishi369@gmail.com>
2026-03-13 20:58:27 -0400

    Added a camera and HemisphericLight

--- js/script.js
+++ js/script.js
@@ -7,6 +7,26 @@ const engine = new BABYLON.Engine(canvas, true);
 // Create
 const createScene = async function () {
 const scene = new BABYLON.Scene(engine);
+
+// Create a camera
+const camera = new BABYLON.ArcRotateCamera(
+"camera",
+-Math.PI / 2,
+Math.PI / 2.5,
+15,
+new BABYLON.Vector3(0,0,0)
+);
+camera.attachControl(canvas,true);
+
+// lighting
+const light = new BABYLON.HemisphericLight(
+"light1",
+new BABYLON.Vector3(0,1,0),
+scene
+);
+
+light.intensity = 0.7;
+
 return scene;
 };
 
